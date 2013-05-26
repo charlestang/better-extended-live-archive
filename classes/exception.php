96a0dc29d3c5e678e3e6ea5e709f9d1d7380a7d3
Charles Tang <charlestang@foxmail.com>
2013-05-26 23:23:16 +0800

    Some comments of the exception file.

--- classes/exception.php
+++ classes/exception.php
@@ -1,9 +1,21 @@
 <?php
-
+/**
+ * This exception will be throwed out when access disk.
+ */
 class BelaIoException extends Exception {
     
 }
 
+/**
+ * This exception will be throwed out when access db.
+ */
 class BelaDbException extends Exception {
+    
+}
+
+/**
+ * This exception will be throwed out when build the index.
+ */
+class BelaIndexException extends Exception {
     
 }
