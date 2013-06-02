d89a7825ebd07ecea470954ee4bf3160d212c927
Charles Tang <charlestang@foxmail.com>
2013-06-02 23:49:16 +0800

    Class rename.

--- classes/Bela.php
+++ classes/Bela.php
@@ -42,7 +42,57 @@ class Bela {
      * Add the hooks to WordPress
      */
     public function registerHooks() {
-        
+        /**
+         * static files
+         */
+        add_action('wp_head', array($this, 'injectStaticFiles'));
+        /**
+         * when post changes, update the index
+         */
+        add_action('publish_post', array($this->builder, 'updateIndexCache'));
+        add_action('deleted_post', array($this->builder, 'updateIndexCache'));
+        /**
+         * when comment changes, update the index
+         */
+        add_action('comment_post', array($this->builder, 'updateIndexCache'));
+        add_action('trackback_post', array($this->builder, 'updateIndexCache'));
+        add_action('pingback_post', array($this->builder, 'updateIndexCache'));
+        add_action('delete_comment', array($this->builder, 'updateIndexCache'));
+        /**
+         * short code
+         */
+        add_shortcode('extended-live-archive', 'af_ela_shorcode');
+
+        if (is_admin()) {
+            if (isset($_GET['page']) && $_GET['page'] == 'extended-live-archive') {
+                add_action('admin_head', 'better_ela_js_code_in_admin_page');
+            }
+            add_action('admin_menu', 'af_ela_admin_pages');
+        }
+    }
+
+    public function injectStaticFiles() {
+        global $ela_plugin_basename;
+        // loading stuff
+        $settings = get_option('af_ela_options');
+        $plugin_path = WP_PLUGIN_URL . '/' . $ela_plugin_basename;
+        if ($settings['use_default_style']) {
+            if (file_exists(ABSPATH . 'wp-content/themes/' . get_template() . '/ela.css')) {
+                $csspath = get_bloginfo('template_url') . "/ela.css";
+            } else {
+                $csspath = $plugin_path . "/includes/af-ela-style.css";
+            }
+
+            $text = <<<TEXT
+
+	<link rel="stylesheet" href="$csspath" type="text/css" media="screen" />
+
+TEXT;
+        } else {
+            $text = '';
+        }
+
+        echo $text;
     }
 
 }
