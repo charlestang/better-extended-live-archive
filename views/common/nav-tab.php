718b940d7f4c58f3e1632e1cb9f3a152fa307878
Charles Tang <charlestang@foxmail.com>
2013-06-06 02:22:16 +0800

    Add navigate tabs on the admin page.

--- views/common/nav-tab.php
+++ views/common/nav-tab.php
@@ -1,8 +1,9 @@
 <div class="icon32" id="icon-bela"><br></div>
 <h2>Better Extended Live Archive Options</h2>
 <ul class="subsubsub">
-    <li class="all"><a class="current" href="plugins.php?plugin_status=all">All <span class="count">(5)</span></a> |</li>
-    <li class="active"><a href="plugins.php?plugin_status=active">Active <span class="count">(3)</span></a> |</li>
-    <li class="inactive"><a href="plugins.php?plugin_status=inactive">Inactive <span class="count">(2)</span></a></li>
+    <li><a href="">What to show </a> |</li>
+    <li><a class="current" href="">How to show </a> |</li>
+    <li><a href="">How to cut </a> |</li>
+    <li><a href="">What about menu </a></li>
 </ul>
 <div class="clear"></div>
